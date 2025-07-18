27dfba3d4a93de290ba2be1f11d3b8348bfedac1
Raiven <rjohns42@hawk.iit.edu>
2025-07-18 14:23:46 -0400

    initial files

--- config/plugins.ts
+++ config/plugins.ts
@@ -17,17 +17,4 @@ export default ({ env }) => ({
       },
     },
   },
-  upload: {
-    config: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-        upload: {
-          folder: 'strapi-media', // optional default folder
-        },
-      },
-    },
-  },
 });
